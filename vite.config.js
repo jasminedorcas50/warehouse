91305071a42e46661b086c29b5aeb00c9ba4854c
Jasmine Dorcas <your.email@example.com>
2025-07-14 01:13:55 -0700

    Allow Render host in Vite preview config

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/theme.css' , 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
     ],
+    preview: {
+        host: true,
+        port: 4173, // You can leave this or set it as needed
+        allowedHosts: ['warehouse-2-7l05.onrender.com'],
+    },
 });
